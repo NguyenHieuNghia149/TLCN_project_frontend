cd3b2b35089a9ad370cac9b0d0ea39a59084751a
NguyenHieuNghia149 <hieunghia484@gmail.com>
2025-12-31 05:42:12 +0700

    ref: update light mode for client ui

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
   baseURL:
-    import.meta.env.REACT_APP_API_URL || 'https://api.algoforge.site/api',
-  //import.meta.env.REACT_APP_API_URL || 'http://localhost:3001/api',
+    //import.meta.env.REACT_APP_API_URL || 'https://api.algoforge.site/api',
+    import.meta.env.REACT_APP_API_URL || 'http://localhost:3001/api',
   timeout: 10000,
   withCredentials: true,
 } as const
